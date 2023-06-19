cd88731b8234fd7c45faec7fd8c99eb503568852
ABDALHAMID <ABD.VOU@GMAIL.COM>
2023-06-19 18:22:40 +0100

    FAKE PORT NUMBER

--- GPI/database/factories/PortFactory.php
+++ GPI/database/factories/PortFactory.php
@@ -21,7 +21,7 @@ class PortFactory extends Factory
             'mac_address' => fake()->unique()->macAddress(),
             'ip_address' => fake()->ipv4(),
             'masque_reseau' => fake()->randomElement(['255.255.255.0', '255.255.0.0', '255.0.0.0','255.255.255.128','255.255.255.192','255.255.128.0','255.255.192.0','255.255.255.252']),
-            'material' => fake()->numberBetween(1, 5000)
+            'material' => fake()->numberBetween(1, 47990)
         ];
     }
 }
